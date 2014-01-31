95281acf2984354885c6ab98b5cf59c324ab9187
=hmph <=anurag.arnab@gmail.com>
2014-01-31 09:52:47 +0200

    Now to get volume rendering in the render window

--- QtTest/main.cpp
+++ QtTest/main.cpp
@@ -1,12 +1,18 @@
 #include "controlwindow.h"
 #include <QtWidgets/QApplication>
 #include "glWidget.h"
+#include "renderwindow.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	ControlWindow w;
 	w.show();
+
+	RenderWindow r;
+	r.show();
+
+	QObject::connect(&w, SIGNAL(imageFilename(QString)), &r, SLOT(initialiseImages(QString)));
 
 	glWidget glw (256, 256, 109);
 	glw.show();
